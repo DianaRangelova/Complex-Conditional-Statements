5e374319c61c98dc3702abcec589cd93be240519
DianaRangelova <diana_kirilova@mail.bg>
2024-01-26 02:18:47 +0200

    Update

---
+++ P09-Cinema-Simplified/Program.cs
@@ -0,0 +1,21 @@
+﻿string typeOfMovie = Console.ReadLine();
+int countOfTheRows = int.Parse(Console.ReadLine());
+int countOfTheSeatsPerRow = int.Parse(Console.ReadLine());
+
+double seatPrice = 0;
+
+if (typeOfMovie == "Normal")
+{
+    seatPrice = 7.50;
+}
+else if (typeOfMovie == "Premiere")
+{
+    seatPrice = 12.00;
+}
+else if (typeOfMovie == "Discount")
+{
+    seatPrice = 5.00;
+}
+
+double totalPrice = seatPrice * countOfTheRows * countOfTheSeatsPerRow;
+Console.WriteLine($"{totalPrice:F2}");
